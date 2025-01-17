e058af7ab6588d12c5a893875faa109a344619cb
Olivier G <zotri@hotmail.com>
2025-01-17 21:21:08 +0100

    ordered list OK

--- parser/blocks/list.rb
+++ parser/blocks/list.rb
@@ -1,31 +1,6 @@
 require_relative "block"
 require_relative "text"
-
-class Item
-  attr_reader :parent, :children, :indent_level, :sign
-  attr_accessor :content
-
-  def initialize(**args)
-    @tab = args[:tab]
-    @parent = args[:parent]
-    @content = args[:content]
-    @children = args[:children] || []
-    @indent_level = args[:indent_level] || 0
-    @sign = find_sign
-  end
-
-  def find_sign
-    case @indent_level
-    when 0 then "•"
-    when 1 then "◦"
-    else "▪"
-    end
-  end
-
-  def indents = "#{@tab} " * @indent_level
-
-  def indent_width = indents.size
-end
+require_relative "list_item"
 
 class List < Block
   TAB = "  "
@@ -37,42 +12,10 @@ class List < Block
 
   private
 
-  # FORMATING #################################################################
-
-  def format(string)
-    string_items = string.split(/((?:.(?:\\\n)?)*?)\n/).reject(&:empty?)
-
-    last_added = nil
-    string_items.each_with_object([]) do |line, items|
-      left_part = line.slice!(/^\s*[-*+] /)
-
-      unless left_part
-        last_added.content.append_str(" #{line}")
-        next items
-      end
-
-      indent_level = (left_part.size - 2) / 2
-      indent_diff = indent_level - last_added.indent_level if last_added
-
-      parent = indent_level.zero? ? nil : find_parent(last_added, indent_diff)
-      item = Item.new(tab: TAB, indent_level:, parent:, content: Text.new(line))
-      parent ? parent.children << item : items << item
-      last_added = item
-    end
-  end
-
-  def find_parent(item, diff)
-    if diff.positive? then item
-    elsif diff.zero? then item.parent
-    else
-      find_parent(item.parent, diff + 1)
-    end
-  end
-
   # RENDERING #################################################################
 
   def item_to_lines(item, lines)
-    indent_width = TAB.size + item.indents.size + 2
+    indent_width = TAB.size + item.indents.size + item.sign.size
     width = @width - indent_width
     sublines = content_to_lines(content: item.content, width:)
     sublines.each_with_index do |line, i|
@@ -82,11 +25,51 @@ class List < Block
     end
   end
 
-  def list_to_lines(items = @content, lines = [])
+  def list_to_lines(items = @items, lines = [])
     items.each do |item|
       item_to_lines(item, lines)
       list_to_lines(item.children, lines)
     end
     lines
+  end
+
+  # FORMATING #################################################################
+
+  def format(string)
+    @items = []
+    @last_added = nil
+
+    string_items = string.split(/((?:.(?:\\\n)?)*?)\n/).reject(&:empty?)
+    string_items.each do |line|
+      left_part = line.slice!(/^ *([-*+]|\d+[.)]) /)
+
+      unless left_part
+        @last_added.content.append_str(" #{line}")
+        next @items
+      end
+
+      ordered = left_part.match?(/\d/)
+      indent_level = (left_part.size - 2) / 2
+      indent_diff = indent_level - @last_added.indent_level if @last_added
+      parent = indent_level.zero? ? nil : find_parent(@last_added, indent_diff)
+      order_nb = parent ? parent.children.size : @items.size
+      items_opts = {ordered:, tab: TAB, indent_level:, parent:, order_nb:, content: Text.new(line)}
+
+      add_item(parent, items_opts)
+    end
+  end
+
+  def add_item(parent, opts)
+    item = ListItem.new(opts)
+    parent ? parent.children << item : @items << item
+    @last_added = item
+  end
+
+  def find_parent(item, diff)
+    if diff.positive? then item
+    elsif diff.zero? then item.parent
+    else
+      find_parent(item.parent, diff + 1)
+    end
   end
 end
---
+++ parser/blocks/list_item.rb
@@ -0,0 +1,52 @@
+class ListItem
+  attr_reader :parent, :children, :indent_level, :sign
+  attr_accessor :content
+
+  def initialize(opts)
+    @content = opts[:content]
+    @ordered = opts[:ordered]
+    @tab = opts[:tab]
+    @parent = opts[:parent]
+    @children = opts[:children] || []
+    @indent_level = opts[:indent_level] || 0
+    @order_nb = opts[:order_nb]
+    @sign = @ordered ? find_ordered_sign : find_sign
+  end
+
+  def indents = "#{@tab} " * @indent_level
+
+  private
+
+  def to_downcase_roman(n)
+    romans = {x: 10, v: 5}
+    res = ""
+
+    romans.each do |roman, val|
+      nb = n / val
+      n %= val
+      res += roman.to_s * nb
+
+      return "#{res}i#{roman}" if n == val - 1
+    end
+    res + "i" * n
+  end
+
+  def ordered? = @ordered
+
+  def find_ordered_sign
+    case @indent_level
+    when 0 then "#{@order_nb + 1}."
+    when 1 then "#{to_downcase_roman(@order_nb + 1)}."
+    else
+      "#{(@order_nb + 97).chr}."
+    end
+  end
+
+  def find_sign
+    case @indent_level
+    when 0 then "•"
+    when 1 then "◦"
+    else "▪"
+    end
+  end
+end
