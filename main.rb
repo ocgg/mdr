57b7c779051718cf4548415d1450966e886bc0a4
Olivier G <zotri@hotmail.com>
2025-01-23 13:51:55 +0100

    introduce command line args

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 
+require "optparse"
 require_relative "renderer"
 require_relative "parser"
 
@@ -9,12 +10,28 @@ require_relative "parser"
 # Sends these blocks to the Renderer which returns an array of strings,
 # Then displays the lines regarding to the CLI arguments received
 class Main
-  def initialize(md_filepath)
+  def initialize(md_filepath, **opts)
+    opts[:left_margin] && @lm = " " * opts[:left_margin]
     # @last_modified = File.mtime(filepath).strftime("%Y-%m-%d %H:%M")
     @parser = Parser.new(File.read(md_filepath))
     @renderer = Renderer.new(@parser.blocks)
-    puts @renderer.result.join("\n\n")
+    @renderer.result.flatten.each do |line|
+      puts "#{@lm}#{line}\n\n"
+    end
   end
 end
 
-Main.new ARGV.first
+opts = {}
+OptionParser.new do |parser|
+  # parser.banner = "Usage: example.rb [opts]"
+  {
+    left_margin: ["-l", "--margin-left=COLS", Integer, "Left margin"],
+    # right_margin: ["-r", "--margin-right=COLS", Integer, "Right margin"]
+  }.each do |name, args|
+    parser.on(*args) { |val| opts[name] = val }
+  end
+end.parse!
+
+raise "Please specify only one file." if ARGV.size > 1
+
+Main.new(ARGF.filename, **opts)
