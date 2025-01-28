e710526a4a76b52a05ce65a6aae34eb21876b5cb
Olivier G <zotri@hotmail.com>
2025-01-28 12:22:49 +0100

    fix table bug

--- lib/blocks/table.rb
+++ lib/blocks/table.rb
@@ -9,7 +9,7 @@ class Table < Block
     @width = opts[:width]
 
     stylized_rows = @rows.map do |row|
-      row.map { |text| spans_to_string(text.spans) }
+      row.map { |text| spans_to_string(text.spans) unless text.is_a?(Array) }
     end
 
     table = TTY::Table.new(stylized_rows)
@@ -32,7 +32,7 @@ class Table < Block
 
   def format(str)
     @rows = str.split("\n").map.with_index do |row, i|
-      row = row.split("|")[1..]
+      row = row.split("|").reject(&:empty?)
       next row if i == 1 # table separator
 
       if i.zero?
@@ -51,6 +51,11 @@ class Table < Block
       else
         :left
       end
+    end
+    # fill empty cols
+    cols = @rows.map(&:size).max
+    @rows.each do |row|
+      row << [] while row.size < cols
     end
   end
 end
